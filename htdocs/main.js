1bfafb235a96f384db53ff2218a724ef6756c86e
Kim Vandry <vandry@TZoNE.ORG>
2026-02-15 18:41:44 +0000

    Expire display of stale values

--- htdocs/main.js
+++ htdocs/main.js
@@ -4,10 +4,24 @@ const {MaisonClient} = require('codegen/maison_grpc_web_pb.js');
 class Maison {
     constructor (api) {
         this.api = api;
+        this.staleness_expiries = {};
     }
 
     run = () => {
         this.run_livetemp();
+    }
+
+    display_value = (staleness_key, value, lifetime, setter) => {
+        if (this.staleness_expiries.hasOwnProperty(staleness_key)) {
+            clearTimeout(this.staleness_expiries[staleness_key]);
+            delete this.staleness_expiries[staleness_key];
+        }
+        setter(value);
+        if ((value !== null) && (lifetime !== null)) {
+            this.staleness_expiries[staleness_key] = setTimeout(() => {
+                this.display_value(staleness_key, null, null, setter);
+            }, lifetime);
+        }
     }
 
     run_livetemp = () => {
@@ -15,13 +29,21 @@ class Maison {
         var req = new proto.google.protobuf.Empty();
         var stream = this.api.monitorLiveTemperatures(req, {});
         stream.on('data', function(response) {
-            var els = document.getElementsByClassName("climate-" + response.getUnit());
-            for (var i = 0; i < els.length; i++) {
-                var livetemp_els = els[i].getElementsByClassName("livetemp");
-                for (var j = 0; j < livetemp_els.length; j++) {
-                    livetemp_els[j].textContent = response.getTemperature().toFixed(1) + " \xb0C";
+            var key = 'climate-' + response.getUnit();
+            top.display_value(
+                key,
+                response.hasTemperature() ? response.getTemperature() : null,
+                4500000,
+                (v) => {
+                    var els = document.getElementsByClassName(key);
+                    for (var i = 0; i < els.length; i++) {
+                        var livetemp_els = els[i].getElementsByClassName("livetemp");
+                        for (var j = 0; j < livetemp_els.length; j++) {
+                            livetemp_els[j].textContent = v == null ? '' : (v.toFixed(1) + " \xb0C");
+                        }
+                    }
                 }
-            }
+            );
         });
         stream.on('status', function(status) {
             setTimeout(() => { top.run_livetemp(); }, 1000);
